8cfdc3fd3c80f020294a380d1ada179bd3b8cd94
Adam Hall <adamhalldesigns@gmail.com>
2022-09-05 10:07:09 +0100

    offline github

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,52 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { ApolloServer } from 'apollo-server';
-import { typeDefs, resolvers } from './schema';
-import { type Services, servicesFactory } from './services';
+import casual from 'casual';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  ApolloServerPluginLandingPageDisabled,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from 'apollo-server-core';
 
-export interface Context {
-  services: Services;
-}
+const c = casual.functions();
+
+const typeDefs = readFileSync(join(__dirname, '../../github-schema.graphql')).toString();
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => c.word(),
+  URI: () => c.url(),
+  User: () => ({
+    company: c.company_name(),
+    avatarUrl: c.url(),
+  }),
+  DateTime: () => c.unix_time(),
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  mocks,
+  mockEntireSchema: true,
   csrfPrevention: true,
-  context: (): Context => ({
-    services: servicesFactory(),
-  }),
+  plugins: [
+    ApolloServerPluginLandingPageDisabled(),
+    ApolloServerPluginLandingPageGraphQLPlayground({}),
+    {
+      async requestDidStart(requestContext) {
+        console.log('request started');
+        console.log(requestContext.request.http?.headers.get('Authorization'));
+        // console.log(requestContext.request.query);
+        // console.log(requestContext.request.variables);
+        return Promise.resolve();
+      },
+    },
+  ],
+  debug: true,
+  logger: console,
 });
 
-server.listen().then((d) => {
+server.listen(4001).then((d) => {
   // eslint-disable-next-line no-console
   console.log(`server ready at ${d.url}`);
 });
